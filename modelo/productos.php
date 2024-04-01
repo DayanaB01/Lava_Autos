35d20622f25342031dcd1193831a1d16e5b03b91
Dayana <dahianab01@gmail.com>
2024-04-01 17:18:06 -0500

    CRUD productos-admin

--- modelo/productos.php
+++ modelo/productos.php
@@ -39,4 +39,62 @@ class productos{
  
 
     }
+
+    function consultarUnico($parametro){
+        extract($parametro);
+        $sql = "SELECT * FROM productos WHERE codigo_producto=?";
+        $rs = $conexion->getPDO()->prepare($sql);
+        if ($rs->execute(array($codigo))) {
+            if ($element = $rs->fetchAll(PDO::FETCH_ASSOC)) {
+                foreach($element as $element){
+                    $array[] = $element;
+                }
+            }
+        }
+        echo json_encode(($array));
+    }
+
+    function editarProducto($parametros){
+        extract($parametros);
+        $sql="UPDATE productos SET nombre_producto='$nombre', tamano_producto='$tamano', valor_producto= '$valor', imagen='$url' WHERE codigo_producto = ?";
+        $rs = $conexion->getPDO()->prepare($sql);
+        if ($rs->execute(array($codigo))) {
+            if ($element = $rs->fetchAll(PDO::FETCH_ASSOC)) {
+                foreach($element as $element){
+                    $array[] = $element;
+                }
+            }
+        }
+        echo json_encode(($array));
+    }
+
+    function eliminarProducto($parametro){
+        extract($parametro);
+        $sql = "DELETE FROM productos WHERE codigo_producto=?";
+        $rs = $conexion->getPDO()->prepare($sql);
+        // $conexion->getPDO()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // try {
+        //     $rs->execute(array($codigo));
+        //     $estado = "Producto eliminado con exito";
+        //     echo json_encode($estado)
+        // } catch (Exception $ex) {
+        //     $estado = "Hubo un problema para eliminar";
+        //     echo json_encode($estado)
+        // }
+        if ($rs->execute(array($codigo))) {
+        try {
+            $estado = "Producto eliminado con exito";
+           // echo json_encode($estado)
+        } catch (Exception $ex) {
+            $estado = "Hubo un problema para eliminar".$ex;
+            //echo json_encode($estado)
+        }
+            // if ($elemento = $rs->fetchAll(PDO::FECTH_ASSOC)) {
+            //     foreach ($elemento as $elemento) {
+            //         $array[] = $elemento;
+            //     }
+            // }
+        } 
+        echo json_encode(($estado));
+    }
 }
--- vista/js/productos.js
+++ vista/js/productos.js
@@ -6,9 +6,26 @@ document.getElementById("registrar").addEventListener('click', function() {
 });
 
 document.getElementById("consultar").addEventListener('click', function() {
-    Consultarproductos();
+    Consultarproductos()   
 });
+document.getElementById("buscarProducto").addEventListener('click', function(){
+    if (document.getElementById('codigo').value == '') {
+        alert('DIGITA UN CODIGO')
+    } else {
+        Consultarproductos();
+        limpiarInputs();
+    }
+});
+// document.getElementById('editarProducto').addEventListener('click', function(){
+//     EditarProducto();
+// })
 
+function limpiarInputs(){
+    let vacio = '';
+    let input = document.getElementById('codigo');
+    input.value = vacio;
+    input.focus();
+}
 
 function AgregarProductos() {
     const data = new FormData();
@@ -29,26 +46,99 @@ function AgregarProductos() {
 
 }
 
+//COMO LIMPIAR EL INPUT
 function Consultarproductos() {
+    let codigo = document.getElementById('codigo').value;
     const data = new FormData();
-    data.append('oper', 'consultarproduc');
-    data.append('clase', 'productos');
+    if (codigo == '') {
+        data.append('oper', 'consultarproduc');
+        data.append('clase', 'productos');  
+    } else {
+        data.append('oper', 'consultarUnico');
+        data.append('clase', 'productos');
+        data.append('codigo', codigo);
+    }
+
     fetch(url, {
             method: 'POST', // or 'PUT'
             body: data,
         }).then(res => res.json())
         .catch(error => console.error('Error:', error))
         .then(response => {
-            //console.log(response);
-            var html="";
-            response.forEach(element => {
-                html+="<tr><th>"+ element.codigo_producto +" </th>" +"<th>"+ element.nombre_producto+" </th>" +"<th>"+ element.tamano_producto+" </th>" 
-                +"<th>"+ element.valor_producto+"</th><th>"+element.imagen+"</th></tr>"
-            });
-            document.getElementById("conproductos").innerHTML=(html);
-        });
-
+            if (response == null) {
+                alert('Este producto no existe')
+            } else {
+                var html="";
+                response.forEach(element => {
+                    html+="<tr><th>"+ element.codigo_producto +" </th>" +"<th>"+ element.nombre_producto+" </th>" +"<th>"+ element.tamano_producto+" </th>" 
+                    +"<th>"+ element.valor_producto+"</th><th>"+element.imagen+"</th><th>"+"<button onclick='EditarProducto("+element.codigo_producto+")'>✍️</button></th><th><button onClick='eliminarProducto("+element.codigo_producto+")'>🗑️</button></th></tr>"
+                });
+    
+                if (codigo == '') {
+                    document.getElementById("conproductos").innerHTML=(html); 
+                } else {
+                    document.getElementById("productoUnico").innerHTML=(html)
+                }
+            }
+        })
 }
 
+function EditarProducto(codigoProducto){
+    const data = new FormData();
 
+    data.append('oper','consultarUnico');
+    data.append('clase','productos');
+    data.append('codigo', codigoProducto)
 
+    fetch(url, {
+        method: 'POST',
+        body: data,
+    }).then(respon => respon.json())
+    .catch(error=>console.error('X',error))
+    .then(
+        response=>{
+            var inputs='';
+
+            response.forEach(e=>{
+                inputs+="<input id='name' value="+e.nombre_producto+"><select id='tamano'><option disabled selected >"+e.tamano_producto+
+                "</option><option value='Grande'>Grande</option><option value='Mediana'>Mediano</option><option value='Pequeno'>Pequeño</option></select><input id='valor' value="
+                +e.valor_producto+"><input id='imagen' value="+e.imagen+
+                "><button onClick='editarProducto("+e.codigo_producto+")'>EDITAR</button>";
+            });
+            document.getElementById('editarInfo').innerHTML=(inputs);
+        }
+    )
+}
+""
+function editarProducto(valor){
+  let data = new FormData();
+  data.append('oper', 'editarProducto');
+  data.append('clase', 'productos')
+  data.append('codigo', valor);
+  data.append('nombre', document.getElementById('name').value);
+  data.append('tamano',document.getElementById('tamano').value);
+  data.append('valor',document.getElementById('valor').value);
+  data.append('url', document.getElementById('imagen').value);
+    fetch(url,{
+        method:'POST',
+        body: data,
+    })
+    .then(res=>res.json())
+    .catch(error=>console.error(error))
+    .then(alert("PRODUCTO EDITADO CON ÉXITO"))
+}
+
+function eliminarProducto(parametro) {
+    let data = new FormData();
+    data.append('oper', 'eliminarProducto');
+    data.append('clase', 'productos');
+    data.append('codigo', parametro);
+    console.log("PRODUCTO:", parametro)
+    fetch(url,{
+        method: 'POST',
+        body: data,
+    })
+    .then(res=>res.json())
+    .catch(error=>console.error(error))
+    .then(alert("ELEMENTO ELIMINADO"))
+}
